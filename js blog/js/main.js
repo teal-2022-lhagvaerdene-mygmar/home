6bab31858b65029b2ffe9563e5fb07d33a54b6f1
teal-2022-lhagvaerdene-mygmar <teal.2022.lhagvaerdene.mygmar@gmail.com>
2022-12-27 17:35:22 +0800

    12-27

--- js blog/js/main.js
+++ js blog/js/main.js
@@ -1,9 +1,17 @@
-fetch('https://jsonplaceholder.typicode.com/posts')
-.then((res) => {
-    if(res.ok){
-        return res.json()
-    }
-})
+fetch('https://dummyjson.com/posts')
+.then((res) => res.json())
 .then((data) => {
-    console.log(data)
+    const one = data.posts.filter((item1, index) => index < 1  )
+    console.log(one)
+    const onep = one
+    .map(
+        (item1) =>
+                `<p> 
+                    <a href="single.html?id=${item1.id}">
+                    ${item1.title}
+                    </a>
+                </p>`
+    ).join('')
+    document.getElementById('card-para').innerHTML = onep
+
 })
