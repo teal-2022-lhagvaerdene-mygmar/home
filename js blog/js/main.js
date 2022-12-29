fae86e3851092a8da018065d8e1e70db66639a11
teal-2022-lhagvaerdene-mygmar <teal.2022.lhagvaerdene.mygmar@gmail.com>
2022-12-29 16:40:52 +0800

    12-29

--- js blog/js/main.js
+++ js blog/js/main.js
@@ -1,13 +1,16 @@
+const query = window.location.search
+const page = query.split('=')
+const size = 5
+console.log(query)
 fetch('https://dummyjson.com/users')
 .then((res) => res.json())
 .then((data) => {
-    const one = data.users.filter((item1, index) => index < 5)
-    console.log(data.users)
+    const one = data.filter((item1, index) => index >= 0 && index <5)
+    // console.log(data.users)
     console.log(one)
     const onep = one
-    .map(
-        (item1) =>
-                `
+    .map(item1 =>
+               `
                 <li id="js"> 
                     <a href="simple.html?id=${item1.id}">
                     ${item1.lastName}
@@ -16,4 +19,4 @@ fetch('https://dummyjson.com/users')
     ).join('')
     document.getElementById('card-para').innerHTML = onep
 
-})
+})  
